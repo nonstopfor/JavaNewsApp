c2e64ef762997369510a58d4be9f19f2f8fe5ee1
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-09-03 17:16:03 +0800

    add presenter and view

---
+++ app/src/main/java/com/java/zhangzhexin/news/newslist/NewsListPresenter.java
@@ -0,0 +1,8 @@
+package com.java.zhangzhexin.news.newslist;
+
+import com.java.zhangzhexin.BasePresenter;
+import com.java.zhangzhexin.BaseView;
+
+public class NewsListPresenter extends BasePresenter<NewsListView> {
+
+}
---
+++ app/src/main/java/com/java/zhangzhexin/news/newslist/NewsListView.java
@@ -0,0 +1,7 @@
+package com.java.zhangzhexin.news.newslist;
+
+import com.java.zhangzhexin.BaseView;
+
+public interface NewsListView extends BaseView {
+
+}
