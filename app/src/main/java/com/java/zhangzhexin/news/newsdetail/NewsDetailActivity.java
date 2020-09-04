d922d983dff49130218da7eba99ffdc4a58a58d3
v_jxwen <v_jxwen@tencent.com>
2020-09-04 19:12:19 +0800

    update activity_detail.xml

--- app/src/main/java/com/java/zhangzhexin/news/newsdetail/NewsDetailActivity.java
+++ app/src/main/java/com/java/zhangzhexin/news/newsdetail/NewsDetailActivity.java
@@ -26,6 +26,8 @@ FIXME:
 public class NewsDetailActivity extends BaseActivity<NewsDetailView,NewsDetailPresenter> implements NewsDetailView  {
     private TextView news_title;
     private TextView news_content;
+    private TextView news_source;
+    private TextView news_date;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +37,8 @@ public class NewsDetailActivity extends BaseActivity<NewsDetailView,NewsDetailPr
         //TODO:news_detail.xml完善
         news_title = findViewById(R.id.news_title);
         news_content = findViewById(R.id.news_content);
+        news_source = findViewById(R.id.news_source);
+        news_date = findViewById(R.id.news_date);
 //        news_title = findViewById(R.id.text_detail);
 //        news_content = findViewById(R.id.text_content);
 
@@ -66,7 +70,7 @@ public class NewsDetailActivity extends BaseActivity<NewsDetailView,NewsDetailPr
         //news_content.setText("这是一条新闻正文");
         news_title.setText(news.title);
         news_content.setText(news.content);
-//        System.out.println("detail setNews finish!");
-        //FIXME:没显示
+        news_date.setText(news.time);
+        news_source.setText(news.source);
     }
 }
