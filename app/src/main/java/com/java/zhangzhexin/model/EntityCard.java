c3c1580f87906059f14b2982d30b1f95cb2f653f
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-09-06 08:39:34 +0800

    add entity definition

---
+++ app/src/main/java/com/java/zhangzhexin/model/EntityCard.java
@@ -0,0 +1,22 @@
+package com.java.zhangzhexin.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class EntityCard {
+    String label;
+    String description;
+    double hot;
+    Map properties;
+    String imgUrl;
+    List<Relation> relationList;
+
+    EntityCard(String label, String description, double hot, Map properties, String imgUrl, List<Relation> relationList) {
+        this.label = label;
+        this.description = description;
+        this.hot = hot;
+        this.properties = properties;
+        this.imgUrl = imgUrl;
+        this.relationList = relationList;
+    }
+}
---
+++ app/src/main/java/com/java/zhangzhexin/model/EntityManager.java
@@ -0,0 +1,12 @@
+package com.java.zhangzhexin.model;
+
+import java.util.List;
+
+public class EntityManager {
+//    List<EntityCard> entityCardList;
+
+    List<EntityCard> getEntityCardList(String keyword){
+        return null;
+    }
+
+}
