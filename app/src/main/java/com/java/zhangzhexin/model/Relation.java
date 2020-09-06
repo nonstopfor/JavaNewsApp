9c0f0758eb578b2112aba17de898a88ce8865ee8
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-09-06 15:46:12 +0800

    add interface for getting entity info

--- app/src/main/java/com/java/zhangzhexin/model/Relation.java
+++ app/src/main/java/com/java/zhangzhexin/model/Relation.java
@@ -5,4 +5,9 @@ public class Relation {
     String label;
     boolean forward;
 
+    public Relation(String relation, String label, boolean forward) {
+        relation = relation;
+        label = label;
+        forward = forward;
+    }
 }
