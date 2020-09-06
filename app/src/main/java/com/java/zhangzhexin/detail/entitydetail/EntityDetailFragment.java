787d63762790889bb136de7309cc12efb5e43c5a
v_jxwen <v_jxwen@tencent.com>
2020-09-06 21:23:33 +0800

    fix entitydetail

--- app/src/main/java/com/java/zhangzhexin/detail/entitydetail/EntityDetailFragment.java
+++ app/src/main/java/com/java/zhangzhexin/detail/entitydetail/EntityDetailFragment.java
@@ -32,7 +32,7 @@ public class EntityDetailFragment extends BaseFragment<EntityDetailView, EntityD
         if(view == null)
         {
             System.out.println("ListFragment: "+type+" view为空");
-            view = inflater.inflate(R.layout.fragment_newsdetail, container, false);
+            view = inflater.inflate(R.layout.fragment_entitydetail, container, false);
             initView();
         }
         return view;
