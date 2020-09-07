70280285087a19106e7ad39a7bcb9fceb64aadbc
v_jxwen <v_jxwen@tencent.com>
2020-09-07 15:01:22 +0800

    call initWeibo()

--- app/src/main/java/com/java/zhangzhexin/detail/DetailActivity.java
+++ app/src/main/java/com/java/zhangzhexin/detail/DetailActivity.java
@@ -140,7 +140,7 @@ public class DetailActivity extends AppCompatActivity implements WbShareCallback
         setContentView(R.layout.activity_detail);
         initFragment();
 
-        //initWeibo();
+        initWeibo();
         initWX();
 
         weixinButton = findViewById(R.id.weixinShareButton);
