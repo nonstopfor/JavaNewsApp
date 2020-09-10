56e1ea80144e9fa7c21d343c1fe66048719fd33b
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-09-10 08:37:19 +0800

    add scholar interface

--- app/src/main/java/com/java/zhangzhexin/model/ScholarManager.java
+++ app/src/main/java/com/java/zhangzhexin/model/ScholarManager.java
@@ -3,10 +3,17 @@ package com.java.zhangzhexin.model;
 import java.util.List;
 
 public class ScholarManager extends BaseManager {
-    List<ScholarCard> scholarCardList;
+    public static List<ScholarCard> scholarCardList;
 
     public List<ScholarCard> refresh() throws InterruptedException {
         scholarCardList = UrlManager.getScholars();
+        for (int i = 0; i < scholarCardList.size(); ++i) {
+            scholarCardList.get(i).idx = i;
+        }
         return scholarCardList;
+    }
+
+    public static ScholarCard getScholar(int idx) {
+        return scholarCardList.get(idx);
     }
 }
