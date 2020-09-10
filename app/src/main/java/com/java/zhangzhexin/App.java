3136eb161ad02b14cbdc6aee7c277dcdd1ee37f7
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-09-10 19:52:58 +0800

    add download loop for news search

--- app/src/main/java/com/java/zhangzhexin/App.java
+++ app/src/main/java/com/java/zhangzhexin/App.java
@@ -4,11 +4,15 @@ import android.app.Application;
 
 import com.java.zhangzhexin.model.DaoMaster;
 import com.java.zhangzhexin.model.DaoSession;
+import com.java.zhangzhexin.model.NewsCard;
 import com.java.zhangzhexin.model.NewsDataManager;
 import com.java.zhangzhexin.model.NewsSearchManager;
+import com.java.zhangzhexin.model.ScholarManager;
 import com.java.zhangzhexin.model.UrlManager;
 
 import org.greenrobot.greendao.database.Database;
+
+import java.util.List;
 
 public class App extends Application {
 
@@ -28,12 +32,21 @@ public class App extends Application {
             @Override
             public void run() {
                 long startTime = System.currentTimeMillis();
-                try {
-                    NewsSearchManager.preDownloadNewsList = UrlManager.getNewsList("news", 1, 1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                int siz = 500;
+                for (int page = 1; ; ++page) {
+                    try {
+                        List<NewsCard> cards = UrlManager.getNewsList("news", page, siz);
+//                        NewsSearchManager.preDownloadNewsList = UrlManager.getNewsList("news", 1, 1000);
+                        NewsSearchManager.preDownloadNewsList.addAll(cards);
+                        System.out.println("download page:" + page);
+//                        System.out.println("cards.size:" + cards.size());
+                        if (cards.isEmpty()) break;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
                 long endTime = System.currentTimeMillis();
+
                 System.out.println("finish thread run, took " + (endTime - startTime) + " ms");
             }
         }.start();
--- app/src/main/java/com/java/zhangzhexin/model/NewsSearchManager.java
+++ app/src/main/java/com/java/zhangzhexin/model/NewsSearchManager.java
@@ -6,14 +6,16 @@ import java.util.List;
 import static java.lang.Thread.sleep;
 
 public class NewsSearchManager {
-    public static List<NewsCard> preDownloadNewsList;
+    public static List<NewsCard> preDownloadNewsList = new ArrayList<>();
 
     public static List<NewsCard> searchNews(String key) throws InterruptedException {
         while (preDownloadNewsList == null) {
             sleep(500);
         }
         List<NewsCard> result = new ArrayList<>();
-        for (NewsCard card : preDownloadNewsList) {
+        int len = preDownloadNewsList.size();
+        for (int i = 0; i < len; ++i) {
+            NewsCard card = preDownloadNewsList.get(i);
             if (card.title.contains(key)) {
                 result.add(card);
             }
